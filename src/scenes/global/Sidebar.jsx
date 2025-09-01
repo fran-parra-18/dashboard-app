6ba73cd818a8d94097493b57cfafeedbced81fc7
fran-parra-18 <98franciscoparra@gmail.com>
2025-09-01 16:20:39 -0300

    first commit

---
+++ src/scenes/global/Sidebar.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { ProSideBar, Menu, MenuItem } from "react-pro-sidebar";
+
+const Sidebar = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Sidebar;
