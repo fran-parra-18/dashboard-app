473a670e0d99fee5bf2440dbc2ab9d392a1f258d
fran-parra-18 <142367632+fran-parra-18@users.noreply.github.com>
2025-09-09 11:56:46 -0300

    Add FAQ page

---
+++ src/scenes/faq/index.jsx
@@ -0,0 +1,86 @@
+import { Box, useTheme, Typography } from "@mui/system";
+import Header from "../../components/Header"
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import { ExpandMoreIcon } from "@mui/icons-material/ExpandMore";
+import { tokens } from "../../theme";
+import { Typography, Typography } from "@mui/material";
+
+const FAQ = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode)
+
+    return (
+        <Box m="20px">
+            <Header title="FAQ" subtitle="Frequently asked questions page"/>
+
+            <Accordion defaultExpanded>
+                <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+                    <Typography color={colors.greenAccent[500]} variant="h5">
+                        an important question
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium sit non totam porro numquam quo nesciunt quasi! Facilis ullam, aperiam quam sunt officia eaque consequatur nobis magnam amet soluta hic?
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+
+            <Accordion defaultExpanded>
+                <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+                    <Typography color={colors.greenAccent[500]} variant="h5">
+                        an important question
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium sit non totam porro numquam quo nesciunt quasi! Facilis ullam, aperiam quam sunt officia eaque consequatur nobis magnam amet soluta hic?
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+
+            <Accordion defaultExpanded>
+                <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+                    <Typography color={colors.greenAccent[500]} variant="h5">
+                        an important question
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium sit non totam porro numquam quo nesciunt quasi! Facilis ullam, aperiam quam sunt officia eaque consequatur nobis magnam amet soluta hic?
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+
+            <Accordion defaultExpanded>
+                <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+                    <Typography color={colors.greenAccent[500]} variant="h5">
+                        an important question
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium sit non totam porro numquam quo nesciunt quasi! Facilis ullam, aperiam quam sunt officia eaque consequatur nobis magnam amet soluta hic?
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+
+            <Accordion defaultExpanded>
+                <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+                    <Typography color={colors.greenAccent[500]} variant="h5">
+                        an important question
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium sit non totam porro numquam quo nesciunt quasi! Facilis ullam, aperiam quam sunt officia eaque consequatur nobis magnam amet soluta hic?
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+        </Box>
+    )
+}
+
+export default FAQ;
